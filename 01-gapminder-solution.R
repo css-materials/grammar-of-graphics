7c522dc9dd01145c22d650527fa267d91a66571d
Sabrina Nardin <nardin@uchicago.com>
2023-10-03 18:32:02 -0500

    updated questions and solutions

--- 01-gapminder-solution.R
+++ 01-gapminder-solution.R
@@ -1,4 +1,7 @@
-# load required packages
+# PRACTICE USING GGPLOT2
+# book: https://r4ds.had.co.nz/data-visualisation.html#data-visualisation
+
+# load required packages (if you are using R locally, install them first)
 library(tibble)
 library(ggplot2)
 library(gapminder)
@@ -9,55 +12,68 @@ gapminder
 # view help file for the dataset
 ?gapminder
 
+
+# replace the _____ with the correct syntax
+# for each question, spend a minute to interpret the plot
+
+
 # generate a histogram of life expectancy
 ggplot(data = gapminder, mapping = aes(x = lifeExp)) +
   geom_histogram()
 
+
+# generate the same histogram, adjusting bins as you prefer
+# note geom_histogram() uses the stat_bin() layer
+# see https://ggplot2.tidyverse.org/reference/geom_histogram.html
+ggplot(data = gapminder, mapping = aes(x = lifeExp)) +
+  geom_histogram(bins = 50) # number of bins
+
+ggplot(data = gapminder, mapping = aes(x = lifeExp)) +
+  geom_histogram(binwidth = 50) # width of each bin
+
+
 # generate separate histograms of life expectancy for each continent
-# Hint: think about how to split your plots to show different subsets of data
+# hint: think how to split your plots to show different subsets of data
 ggplot(data = gapminder, mapping = aes(x = lifeExp)) +
   geom_histogram() +
   facet_wrap(facets = vars(continent))
 
-# compare the distribution of life expectancy, by continent by generating a boxplot
+
+# compare the distribution of life expectancy by continent, by generating a boxplot
+# to interpret see https://www.mathbootcamps.com/how-to-read-a-boxplot/
 ggplot(data = gapminder, mapping = aes(x = continent, y = lifeExp)) +
   geom_boxplot()
 
-## redraw the plot, but this time use a violin plot
-ggplot(data = gapminder, mapping = aes(x = continent, y = lifeExp)) +
-  geom_violin()
 
 # generate a scatterplot of the relationship between per capita GDP and life expectancy
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point()
 
-## add a smoothing line to the scatterplot
+
+# add a smoothing line to the scatterplot using the "lm" method 
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
   geom_point() +
-  geom_smooth()
+  geom_smooth(method = "lm")
 
-## use color to identify whether this relationship differs by continent
+
+# use the color aesthetic to identify whether this relationship differs by continent
 ggplot(data = gapminder,
        mapping = aes(x = gdpPercap, y = lifeExp, color = continent)) +
   geom_point() +
   geom_smooth()
 
-## add a title to the last graph you generated
+
+# add a title to the last graph you generated
 ggplot(data = gapminder,
        mapping = aes(x = gdpPercap, y = lifeExp, color = continent)) +
   geom_point() +
   geom_smooth() +
   ggtitle('GDP per Capita by Continent')
 
-## bonus: identify the outlying countries on the right-side of the graph
-## by labeling each observation with the country name
+
+# identify the outlying countries on the right-side of the graph
+# by labeling each observation with the country name
 ggplot(data = gapminder,
        mapping = aes(x = gdpPercap, y = lifeExp, label = country)) +
   geom_smooth() +
   geom_text()
-  
-## bonus: transform the data to logarithmic scale
-ggplot(data = gapminder,
-       mapping = aes(x = gdpPercap, y = lifeExp, color = continent)) +
-  geom_point() +
-  scale_x_log10()
--- 01-gapminder.R
+++ 01-gapminder.R
@@ -1,4 +1,7 @@
-# load required packages
+# PRACTICE USING GGPLOT2
+# book: https://r4ds.had.co.nz/data-visualisation.html#data-visualisation
+
+# load required packages (if you are using R locally, install them first)
 library(tibble)
 library(ggplot2)
 library(gapminder)
@@ -9,54 +12,62 @@ gapminder
 # view help file for the dataset
 ?gapminder
 
+
+# replace the _____ with the correct syntax
+# for each question, spend a minute to interpret the plot
+
+
 # generate a histogram of life expectancy
 ggplot(data = ________, mapping = aes(x = ________)) +
   geom________()
 
+
+# generate the same histogram, adjusting bins as you prefer
+ggplot(data = ________, mapping = aes(x = ________)) +
+  geom________()
+
+
 # generate separate histograms of life expectancy for each continent
-# Hint: think about how to split your plots to show different subsets of data
+# hint: think how to split plots to show different subsets of data
 ggplot(data = ________, mapping = aes(x = ________)) +
   geom________() +
   ________(facets = vars(________))
 
-# compare the distribution of life expectancy, by continent by generating a boxplot
+
+# compare the distribution of life expectancy by continent, by generating a boxplot
 ggplot(data = ________, mapping = aes(x = ________, y = ________)) +
   geom________()
 
-## redraw the plot, but this time use a violin plot
-ggplot(data = ________, mapping = aes(x = ________, y = ________)) +
-  geom________()
 
 # generate a scatterplot of the relationship between per capita GDP and life expectancy
 ggplot(data = ________, mapping = aes(x = ________, y = ________)) +
   geom________()
 
-## add a smoothing line to the scatterplot
+
+# add a smoothing line to the scatterplot using the "lm" method 
 ggplot(data = ________, mapping = aes(x = ________, y = ________)) +
   geom________() +
   geom________()
 
-## use color to identify whether this relationship differs by continent
+
+# use the color aesthetic to identify whether this relationship differs by continent
 ggplot(data = ________,
        mapping = aes(x = ________, y = ________, color = ________)) +
   geom________() +
   geom________()
 
-## add a title to the last graph you generated
+
+# add a title to the last graph you generated
 ggplot(data = ________,
        mapping = aes(x = ________, y = ________, color = ________)) +
   geom________() +
   geom________() + 
   ggtitle(______________)
   
-## bonus: identify the outlying countries on the right-side of the graph
-## by labeling each observation with the country name
+
+# identify the outlying countries on the right-side of the graph
+# by labeling each observation with the country name
 ggplot(data = ________,
        mapping = aes(x = ________, y = ________, label = ________)) +
   geom________() +
   geom________()
-
-## bonus: transform the data to logarithmic scale
-
-
-
